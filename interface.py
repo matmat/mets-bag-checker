20894175efca86d4c1a3d96b07a188fd4dd2f8fc
Bertrand Caron <bertrand.caron@free.fr>
2023-10-08 13:21:57 +0200

    Adding the project files.

---
+++ interface.py
@@ -0,0 +1,201 @@
+"""Module of the GUI for METS validation & integrity checking."""
+
+#!/usr/bin/python3.10
+# -*-coding:utf-8 -*
+
+
+from os import path
+from time import localtime,strftime
+from math import floor
+import csv
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter.messagebox import showinfo
+
+import mets
+
+class UndefinedDirectory(Exception):
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class UndefinedMETSPattern(Exception):
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class UndefinedAction(Exception):
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class Report():
+
+    """This class is intended for analysis reports"""
+
+    def __init__(self, validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action) -> None:
+        self.actions = []
+        self.date = localtime()
+        if validate_action.get() == '1':
+            self.validation_report = {}
+            self.actions.append('Validation')
+        if checkCompleteness_action.get() == '1'  and (not checkFixity_action.get() or checkFixity_action.get() == '0'):
+            self.completeness_report = {}
+            self.actions.append('Completeness check')
+        if checkFixity_action.get() == '1':
+            self.completenessAndFixity_report = {}
+            self.actions.append('Completeness and fixity check')
+        if checkOrphanness_action.get() == '1':
+            self.orphanness_report = {}
+            self.actions.append('Orphanness check')
+    
+    def __repr__(self) -> str:
+        return (f'Analysis report of the folder {mets.directory} '
+                f'performing {", ".join(action for action in self.actions)}, '
+                f'started at {strftime("%Y-%m-%dT%H:%M:%S%z", self.date)}.')
+
+def define_directory():
+
+    """Asks the user to define the directory where the Information Packages are located."""
+
+    mets.directory = filedialog.askdirectory()
+    directory_label.config(text=mets.directory)
+
+def launch_test(validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action):
+
+    """Calls the selected functions to perform tests selected by the user"""
+
+    # Exceptions if directory and METS file name pattern are not defined.
+    if mets.directory == '':
+        raise UndefinedDirectory('Please define a folder to analyse.')
+    elif mets_pattern.get() == '':
+        raise UndefinedMETSPattern('Please define the METS files name pattern.')
+    elif validate_action.get() != '1' and checkCompleteness_action.get() != '1' and \
+        checkFixity_action.get() != '1' and checkOrphanness_action.get() != '1':
+        raise UndefinedAction('No action is selected.')
+    else:
+        # Detection of the Information Packages based on the METS file name pattern. 
+        mets.mets_pattern = '/**/' + mets_pattern.get()
+        IPs = mets.InformationPackages()
+        IPs.find_information_packages()
+        # Display progress bar
+        progressBar = ttk.Progressbar(frame1, orient='horizontal', mode='determinate', length=280)
+        progressBar.grid(row=8, column=0)
+        progress_label = ttk.Label(frame1, text=f"Current progress: 0%")
+        progress_percentage = float()
+        progress_label.grid(row=9, column=0)
+        # # Display abort button
+        # abortButton = ttk.Button(frame1, text='Abort analysis')
+        # abortButton.grid(row=10, column=0)
+        # Initiate report
+        report = Report(validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action)
+        # Counter to evaluate progression of the analysis process
+        counter = 0
+        for file in IPs.list_of_ips:
+            manifest = mets.METSFile(path.join(file[0], file[1]))
+            # Validation check
+            if validate_action.get() == '1':
+                report.validation_report[manifest.path_to_mets_file] = manifest.validate()
+            # Completeness check alone
+            if checkCompleteness_action.get() == '1' and (not checkFixity_action.get() or checkFixity_action.get() == '0'):
+                report.completeness_report[manifest.path_to_mets_file] = manifest.checkCompleteness()
+            # Completeness check plus fixity check
+            if checkFixity_action.get() == '1':
+                report.completenessAndFixity_report[manifest.path_to_mets_file] = manifest.checkCompletenessAndFixity()
+            # Orphanness check
+            if checkOrphanness_action.get() == '1':
+                report.orphanness_report[manifest.path_to_mets_file] = manifest.checkOrphanness()
+            counter += 1
+            progress_percentage = round(counter / (len(IPs.list_of_ips) / 100), 2)
+            progressBar.config(value=floor(progress_percentage))
+            progress_label.config(text=f'Current progress = {progress_percentage}%.')
+            frame1.update_idletasks()
+        with open(f'Report_{strftime("%Y-%m-%dT%H:%M:%S%z", report.date)}.csv', 'w', newline='') as csvreport:
+            csvwriter = csv.writer(csvreport, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            report.actions.insert(0, 'Package')
+            csvwriter.writerow(report.actions)
+            for IP in IPs.list_of_ips:
+                row = [IP[0]]
+                if 'Validation' in report.actions:
+                    row.append(report.validation_report["/".join(IP)])
+                if 'Completeness check' in report.actions:
+                    row.append(report.completeness_report["/".join(IP)])
+                if 'Completeness and fixity check' in report.actions:
+                    row.append(report.completenessAndFixity_report["/".join(IP)])
+                if 'Orphanness check' in report.actions:
+                    row.append(report.orphanness_report["/".join(IP)])
+                csvwriter.writerow(row)
+        showinfo("Process ended.", f"The process analyzed {len(IPs.list_of_ips)} packages.")
+
+# Interface creation
+root = tk.Tk()
+root.title("METS packages validation")
+root.geometry('1500x1000')
+icon = tk.PhotoImage(file='mets.png')
+root.iconphoto(True, icon)
+
+# Main frame creation
+frame1 = tk.Frame(root, width=700, height=400)
+frame1.pack()
+frame1.columnconfigure(0, weight=1)
+frame1.columnconfigure(1, weight=1)
+
+# Button to select the directory where METS Information Packages are located.
+define_directory_button = ttk.Button(frame1, text="Select a folder for analysis",
+                                     command=define_directory)
+define_directory_button.grid(row=0, column=0)
+define_directory_button.focus()
+
+# Label displaying the chosen directory.
+directory_label = ttk.Label(frame1)
+directory_label.grid(row=0, column=1)
+
+# Label to introduce the METSfilename pattern entry.
+define_metspattern_label = ttk.Label(frame1,
+                                     text="Enter the METS filename or pattern (wildcards are accepted):")
+define_metspattern_label.grid(row=1, column=0)
+
+# Entry for defining the METS filename pattern.
+mets_pattern = tk.StringVar()
+define_metspattern_entry = ttk.Entry(frame1, textvariable=mets_pattern)
+mets_pattern.set('manifest.xml')
+define_metspattern_entry.grid(row=1, column=1)
+
+# Checkboxes to select which operations to perform.
+validate_action = tk.StringVar()
+checkCompleteness_action = tk.StringVar()
+checkFixity_action = tk.StringVar()
+checkOrphanness_action = tk.StringVar()
+checkbox_operation_validate = ttk.Checkbutton(frame1, text="Validate METS manifests",
+                                              variable=validate_action)
+checkbox_operation_checkCompleteness = ttk.Checkbutton(frame1,
+                                                       text="Check all referenced files are present",
+                                                        variable=checkCompleteness_action)
+checkbox_operation_checkFixity = ttk.Checkbutton(frame1, text="Check all referenced files' fixity",
+                                                 variable=checkFixity_action)
+checkbox_operation_checkOrphanness = ttk.Checkbutton(frame1,
+                                                     text=("Check if the packages "
+                                                           "contain unreferenced files"),
+                                                            variable=checkOrphanness_action)
+checkbox_operation_validate.grid(row=2, column=0, sticky=tk.W)
+checkbox_operation_checkCompleteness.grid(row=3, column=0, sticky=tk.W)
+checkbox_operation_checkFixity.grid(row=4, column=0, sticky=tk.W)
+checkbox_operation_checkOrphanness.grid(row=5, column=0, sticky=tk.W)
+
+# Button for launching the analysis.
+launch_analysis_button = ttk.Button(frame1, command=lambda: launch_test(validate_action,
+                                                                        checkCompleteness_action,
+                                                                        checkFixity_action,
+                                                                        checkOrphanness_action),
+                                                                        text="Launch the test")
+launch_analysis_button.grid(row=6, column=0)
+
+# Additional code to correct blurry edges on Windows.
+# try:
+#     from ctypes import windll
+
+#     windll.shcore.SetProcessDpiAwareness(1)
+# finally:
+root.mainloop()
---
+++ mets.py
@@ -0,0 +1,155 @@
+"""Module for batch validation of METS files."""
+
+#!/usr/bin/python3.10
+# -*-coding:utf-8 -*
+from os import path
+import glob
+import sys
+
+from lxml import etree
+import hashlib
+
+
+directory = ''
+mets_pattern = r''
+
+class InformationPackages:
+
+    """This class identifies Information Packages in the target folder.i.e. folders
+     containing a METS file with an expected file name."""
+
+    def __init__(self) -> None:
+        self.list_of_ips = []
+
+    def find_information_packages(self):
+
+        """Scans the target folder and returns all XML files matching the manifest name pattern."""
+
+        for file in glob.glob(directory + mets_pattern, recursive=True):
+            self.list_of_ips.append(path.split(file))
+
+
+class METSFile:
+
+    """This class is intended for XML-METS files."""
+
+    def __init__(self, path_to_mets_file) -> None:
+        self.path_to_mets_file = path_to_mets_file
+        self.directory = path.split(path_to_mets_file)[0]
+        self.name = path.split(path_to_mets_file)[1]
+
+        """Parses the XML file and returns an error if the XML file is not well-formed."""
+
+        try:
+            self.xml = etree.parse(self.path_to_mets_file)
+        except etree.XMLSyntaxError:
+            print(f"Le fichier {self.path_to_mets_file} n'est pas bien formé.", file=sys.stderr)
+
+
+    def validate(self):
+
+        """Validates the METS file against its XSD schema."""
+        parsed_schema = etree.parse('METSSchema/mets.xsd')
+        mets_schema = etree.XMLSchema(parsed_schema)
+        if mets_schema.validate(self.xml) == True:
+            return "Valid"
+        else:
+            return "Invalid"
+    
+    def listReferencedFiles(self):
+
+        """Extracts a list of relative paths referenced in the <fileSec> section of the METS file."""
+        self.list_of_referenced_files = []
+        for file in self.xml.xpath("/mets:mets/mets:fileSec//mets:file/mets:FLocat", 
+                                   namespaces={"mets": "http://www.loc.gov/METS/"}):
+            self.list_of_referenced_files.append(file.xpath("@xlink:href", 
+                                    namespaces={"xlink": "http://www.w3.org/1999/xlink"})[0])
+            
+    def listPackageFiles(self):
+
+        """Returns a list of relative paths to every file located in the same folder as the METS file."""
+        self.list_of_package_files = []
+        for file in glob.glob(self.directory + '**/*', recursive=True):
+            if not path.isdir(file) and not path.basename(file) == self.name:
+                self.list_of_package_files.append(path.basename(file))
+
+    def checkCompleteness(self):
+
+        """Checks all referenced files in the mets:fileSec are present."""
+        missing_files = []
+        self.listReferencedFiles()
+        for file in self.list_of_referenced_files:
+            absolute_path_to_data_object = path.join(self.directory, file)
+            if not path.exists(absolute_path_to_data_object):
+                missing_files.append(file)
+        if missing_files:
+            return f"Incomplete. Files {missing_files} are missing."
+        else:
+            return f"Complete."
+        
+    def checkCompletenessAndFixity(self):
+
+        """Checks all referenced files in the mets:fileSec are present and unaltered."""
+        missing_files = []
+        unchecked_files = []
+        altered_files = []
+        for file in self.xml.xpath("/mets:mets/mets:fileSec//mets:file/mets:FLocat", 
+                                   namespaces={"mets": "http://www.loc.gov/METS/"}):
+            relative_path_to_data_object = file.xpath("@xlink:href", 
+                                    namespaces={"xlink": "http://www.w3.org/1999/xlink"})[0]
+            absolute_path_to_data_object = path.join(self.directory, relative_path_to_data_object)
+            if not path.exists(absolute_path_to_data_object):
+                missing_files.append(relative_path_to_data_object)
+            elif file.xpath("../@CHECKSUM") and file.xpath("../@CHECKSUMTYPE"):
+                BUF_SIZE = 65536
+                if file.xpath("../@CHECKSUMTYPE")[0] == "MD5":
+                    checksum = hashlib.md5()
+                elif file.xpath("../@CHECKSUMTYPE")[0] == "SHA-1":
+                    checksum = hashlib.sha1()
+                elif file.xpath("../@CHECKSUMTYPE")[0] == "SHA-256":
+                    checksum = hashlib.sha256()
+                elif file.xpath("../@CHECKSUMTYPE")[0] == "SHA-384":
+                    checksum = hashlib.sha384()
+                elif file.xpath("../@CHECKSUMTYPE")[0] == "SHA-512":
+                    checksum = hashlib.sha512()
+                with open(absolute_path_to_data_object, "rb") as binaryFile:
+                    while True:
+                        data = binaryFile.read(BUF_SIZE)
+                        if not data:
+                            break
+                        checksum.update(data)
+                if checksum.hexdigest() != file.xpath("../@CHECKSUM")[0]:
+                    altered_files.append(relative_path_to_data_object)
+            else:
+                unchecked_files.append(relative_path_to_data_object)
+        if missing_files and not altered_files:
+            return f"Incomplete. Files {missing_files} are missing."
+        if not missing_files and altered_files:
+            return f"Altered. Altered files: {altered_files}."
+        if missing_files and altered_files:
+            return (f"Incomplete and altered. Files {missing_files} are missing. "
+                    f"Altered iles {altered_files}.")
+        else:
+            if not unchecked_files:
+                return f"Unaltered."
+            else:
+                return (f"Valid, but the fixity of files {unchecked_files} "
+                        f"could not be checked.")
+        
+    def checkOrphanness(self):
+
+        """Checks the absence of unreferenced files in the Information Package,
+        i.e., files that are in the same folder as the METS file but that are not referenced
+        in its file section <fileSec>."""
+        orphan_files = []
+        if not hasattr(self, 'list_of_referenced_files'):
+            self.listReferencedFiles()
+        self.listPackageFiles()
+        for file in self.list_of_package_files:
+            if file not in self.list_of_referenced_files:
+                orphan_files.append(file)
+        if orphan_files:
+            return f"Orphan files: {orphan_files}."
+        else:
+            return f"No orphan files."
+
