231ff06bf8a43472be3939df9570bdc90f0ad1eb
Bertrand Caron <bertrand.caron@free.fr>
2023-10-27 14:12:15 +0200

    Report errors to the user in an error box

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ import csv
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo, showerror
 
 import mets
 
@@ -56,150 +56,158 @@ class Report():
                 f'performing {", ".join(action for action in self.actions)}, '
                 f'started at {strftime("%Y-%m-%dT%H:%M:%S%z", self.date)}.')
 
-def define_directory():
+class App(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
 
-    """Asks the user to define the directory where the Information Packages are located."""
+        # Interface creation
+        self.title("METS packages validation")
+        self.geometry('1000x400')
+        self.icon = tk.PhotoImage(file='mets.png')
+        self.iconphoto(True, self.icon)
 
-    mets.directory = filedialog.askdirectory()
-    directory_label.config(text=mets.directory)
+        # Main frame creation
+        # self = tk.Frame(self, width=700, height=400)
+        # self.pack()
+        # self.columnconfigure(0, weight=1)
+        # self.columnconfigure(1, weight=1)
 
-def launch_test(validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action):
+        # Button to select the directory where METS Information Packages are located.
+        self.define_directory_button = ttk.Button(self, text="1. Select a folder for analysis",
+                                            command=self.define_directory)
+        self.define_directory_button.grid(row=0, column=0, sticky=tk.W)
+        self.define_directory_button.focus()
 
-    """Calls the selected functions to perform tests selected by the user"""
+        # Label displaying the chosen directory.
+        self.directory_label = ttk.Label(self)
+        self.directory_label.grid(row=0, column=1)
 
-    # Exceptions if directory and METS file name pattern are not defined.
-    if mets.directory == '':
-        raise UndefinedDirectory('Please define a folder to analyse.')
-    elif mets_pattern.get() == '':
-        raise UndefinedMETSPattern('Please define the METS files name pattern.')
-    elif validate_action.get() != '1' and checkCompleteness_action.get() != '1' and \
-        checkFixity_action.get() != '1' and checkOrphanness_action.get() != '1':
-        raise UndefinedAction('No action is selected.')
-    else:
-        # Detection of the Information Packages based on the METS file name pattern. 
-        mets.mets_pattern = '/**/' + mets_pattern.get()
-        IPs = mets.InformationPackages()
-        IPs.find_information_packages()
-        # Display progress bar
-        progressBar = ttk.Progressbar(frame1, orient='horizontal', mode='determinate', length=280)
-        progressBar.grid(row=10, column=0)
-        progress_label = ttk.Label(frame1, text=f"Current progress: 0%")
-        progress_percentage = float()
-        progress_label.grid(row=11, column=0)
-        # Initiate report
-        report = Report(validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action)
-        # Counter to evaluate progression of the analysis process
-        counter = 0
-        for file in IPs.list_of_ips:
-            manifest = mets.METSFile(path.join(file[0], file[1]))
-            # Validation check
-            if validate_action.get() == '1':
-                report.validation_report[manifest.path_to_mets_file] = manifest.validate()
-            # Completeness check alone
-            if checkCompleteness_action.get() == '1' and (not checkFixity_action.get() or checkFixity_action.get() == '0'):
-                report.completeness_report[manifest.path_to_mets_file] = manifest.checkCompleteness()
-            # Completeness check plus fixity check
-            if checkFixity_action.get() == '1':
-                report.completenessAndFixity_report[manifest.path_to_mets_file] = manifest.checkCompletenessAndFixity()
-            # Orphanness check
-            if checkOrphanness_action.get() == '1':
-                report.orphanness_report[manifest.path_to_mets_file] = manifest.checkOrphanness()
-            counter += 1
-            progress_percentage = round(counter / (len(IPs.list_of_ips) / 100), 2)
-            progressBar.config(value=floor(progress_percentage))
-            progress_label.config(text=f'Current progress = {progress_percentage}%.')
-            frame1.update_idletasks()
-            select_output_location_button = ttk.Button(frame1, text="5. Select the location for the report file.", command=lambda:save_report(report, IPs))
-            select_output_location_button.grid(column=0, row=12)
-        showinfo("Process ended.", f"The process analyzed {len(IPs.list_of_ips)} packages.")
+        # Label to introduce the METSfilename pattern entry.
+        self.define_metspattern_label = ttk.Label(self,
+                                            text="2. Enter the METS filename or pattern:")
+        self.define_metspattern_label.grid(row=2, column=0, sticky=tk.W)
 
-def save_report(report, IPs):
-    location = filedialog.asksaveasfilename(initialdir=path.curdir,
-                                            initialfile=f'Report_{strftime("%Y-%m-%dT%H:%M:%S%z", report.date)}.csv',
-                                            title = "Select the report location.",
-                                            filetypes = [("csv files", "*.csv")])
-    with open(location, 'w', newline='') as csvreport:
-        csvwriter = csv.writer(csvreport, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        report.actions.insert(0, 'Package')
-        csvwriter.writerow(report.actions)
-        for IP in IPs.list_of_ips:
-            row = [IP[0]]
-            if 'Validation' in report.actions:
-                row.append(report.validation_report["/".join(IP)])
-            if 'Completeness check' in report.actions:
-                row.append(report.completeness_report["/".join(IP)])
-            if 'Completeness and fixity check' in report.actions:
-                row.append(report.completenessAndFixity_report["/".join(IP)])
-            if 'Orphanness check' in report.actions:
-                row.append(report.orphanness_report["/".join(IP)])
-            csvwriter.writerow(row)
+        # Entry for defining the METS filename pattern.
+        self.mets_pattern = tk.StringVar()
+        self.define_metspattern_entry = ttk.Entry(self, textvariable=self.mets_pattern)
+        self.mets_pattern.set('manifest.xml')
+        self.define_metspattern_entry.grid(row=2, column=1)
 
+        # Label to introduce the actions selection.
+        self.actions_label = ttk.Label(self, text="3. Select one or more actions.")
+        self.actions_label.grid(row=4, column=0, sticky=tk.W)
+        # Checkboxes to select which operations to perform.
+        self.validate_action = tk.StringVar()
+        self.checkCompleteness_action = tk.StringVar()
+        self.checkFixity_action = tk.StringVar()
+        self.checkOrphanness_action = tk.StringVar()
+        self.checkbox_operation_validate = ttk.Checkbutton(self, text="Validate METS manifests",
+                                                    variable=self.validate_action)
+        self.checkbox_operation_checkCompleteness = ttk.Checkbutton(self,
+                                                            text="Check all referenced files are present",
+                                                                variable=self.checkCompleteness_action)
+        self.checkbox_operation_checkFixity = ttk.Checkbutton(self, text="Check all referenced files' fixity",
+                                                        variable=self.checkFixity_action)
+        self.checkbox_operation_checkOrphanness = ttk.Checkbutton(self,
+                                                            text=("Check if the packages "
+                                                                "contain unreferenced files"),
+                                                                    variable=self.checkOrphanness_action)
+        self.checkbox_operation_validate.grid(row=5, column=0, sticky=tk.W)
+        self.checkbox_operation_checkCompleteness.grid(row=6, column=0, sticky=tk.W)
+        self.checkbox_operation_checkFixity.grid(row=7, column=0, sticky=tk.W)
+        self.checkbox_operation_checkOrphanness.grid(row=8, column=0, sticky=tk.W)
 
-# Interface creation
-root = tk.Tk()
-root.title("METS packages validation")
-root.geometry('1000x400')
-icon = tk.PhotoImage(file='mets.png')
-root.iconphoto(True, icon)
+        # Button for launching the analysis.
+        self.launch_analysis_button = ttk.Button(self, command=lambda: self.launch_test(self.validate_action,
+                                                                                self.checkCompleteness_action,
+                                                                                self.checkFixity_action,
+                                                                                self.checkOrphanness_action),
+                                                                                text="4. Launch the test")
+        self.launch_analysis_button.grid(row=9, column=0)
 
-# Main frame creation
-frame1 = tk.Frame(root, width=700, height=400)
-frame1.pack()
-frame1.columnconfigure(0, weight=1)
-frame1.columnconfigure(1, weight=1)
+    def define_directory(self):
 
-# Button to select the directory where METS Information Packages are located.
-define_directory_button = ttk.Button(frame1, text="1. Select a folder for analysis",
-                                     command=define_directory)
-define_directory_button.grid(row=0, column=0, sticky=tk.W)
-define_directory_button.focus()
+        """Asks the user to define the directory where the Information Packages are located."""
 
-# Label displaying the chosen directory.
-directory_label = ttk.Label(frame1)
-directory_label.grid(row=0, column=1)
+        mets.directory = filedialog.askdirectory()
+        self.directory_label.config(text=mets.directory)
 
-# Label to introduce the METSfilename pattern entry.
-define_metspattern_label = ttk.Label(frame1,
-                                     text="2. Enter the METS filename or pattern:")
-define_metspattern_label.grid(row=2, column=0, sticky=tk.W)
+    def launch_test(self, validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action):
 
-# Entry for defining the METS filename pattern.
-mets_pattern = tk.StringVar()
-define_metspattern_entry = ttk.Entry(frame1, textvariable=mets_pattern)
-mets_pattern.set('manifest.xml')
-define_metspattern_entry.grid(row=2, column=1)
+        """Calls the selected functions to perform tests selected by the user"""
 
-# Label to introduce the actions selection.
-actions_label = ttk.Label(frame1, text="3. Select one or more actions.")
-actions_label.grid(row=4, column=0, sticky=tk.W)
-# Checkboxes to select which operations to perform.
-validate_action = tk.StringVar()
-checkCompleteness_action = tk.StringVar()
-checkFixity_action = tk.StringVar()
-checkOrphanness_action = tk.StringVar()
-checkbox_operation_validate = ttk.Checkbutton(frame1, text="Validate METS manifests",
-                                              variable=validate_action)
-checkbox_operation_checkCompleteness = ttk.Checkbutton(frame1,
-                                                       text="Check all referenced files are present",
-                                                        variable=checkCompleteness_action)
-checkbox_operation_checkFixity = ttk.Checkbutton(frame1, text="Check all referenced files' fixity",
-                                                 variable=checkFixity_action)
-checkbox_operation_checkOrphanness = ttk.Checkbutton(frame1,
-                                                     text=("Check if the packages "
-                                                           "contain unreferenced files"),
-                                                            variable=checkOrphanness_action)
-checkbox_operation_validate.grid(row=5, column=0, sticky=tk.W)
-checkbox_operation_checkCompleteness.grid(row=6, column=0, sticky=tk.W)
-checkbox_operation_checkFixity.grid(row=7, column=0, sticky=tk.W)
-checkbox_operation_checkOrphanness.grid(row=8, column=0, sticky=tk.W)
+        # Exceptions if directory and METS file name pattern are not defined.
+        if mets.directory == '':
+            raise UndefinedDirectory('Please define a folder to analyse.')
+        elif self.mets_pattern.get() == '':
+            raise UndefinedMETSPattern('Please define the METS files name pattern.')
+        elif validate_action.get() != '1' and checkCompleteness_action.get() != '1' and \
+            checkFixity_action.get() != '1' and checkOrphanness_action.get() != '1':
+            raise UndefinedAction('No action is selected.')
+        else:
+            # Detection of the Information Packages based on the METS file name pattern. 
+            mets.mets_pattern = '/**/' + self.mets_pattern.get()
+            IPs = mets.InformationPackages()
+            IPs.find_information_packages()
+            # Display progress bar
+            progressBar = ttk.Progressbar(self, orient='horizontal', mode='determinate', length=280)
+            progressBar.grid(row=10, column=0)
+            progress_label = ttk.Label(self, text=f"Current progress: 0%")
+            progress_percentage = float()
+            progress_label.grid(row=11, column=0)
+            # Initiate report
+            report = Report(validate_action, checkCompleteness_action, checkFixity_action, checkOrphanness_action)
+            # Counter to evaluate progression of the analysis process
+            counter = 0
+            for file in IPs.list_of_ips:
+                manifest = mets.METSFile(path.join(file[0], file[1]))
+                # Validation check
+                if validate_action.get() == '1':
+                    report.validation_report[manifest.path_to_mets_file] = manifest.validate()
+                # Completeness check alone
+                if checkCompleteness_action.get() == '1' and (not checkFixity_action.get() or checkFixity_action.get() == '0'):
+                    report.completeness_report[manifest.path_to_mets_file] = manifest.checkCompleteness()
+                # Completeness check plus fixity check
+                if checkFixity_action.get() == '1':
+                    report.completenessAndFixity_report[manifest.path_to_mets_file] = manifest.checkCompletenessAndFixity()
+                # Orphanness check
+                if checkOrphanness_action.get() == '1':
+                    report.orphanness_report[manifest.path_to_mets_file] = manifest.checkOrphanness()
+                counter += 1
+                progress_percentage = round(counter / (len(IPs.list_of_ips) / 100), 2)
+                progressBar.config(value=floor(progress_percentage))
+                progress_label.config(text=f'Current progress = {progress_percentage}%.')
+                self.update_idletasks()
+                select_output_location_button = ttk.Button(self, text="5. Select the location for the report file.", command=lambda: self.save_report(report, IPs))
+                select_output_location_button.grid(column=0, row=12)
+            showinfo("Process ended.", f"The process analyzed {len(IPs.list_of_ips)} packages.")
 
-# Button for launching the analysis.
-launch_analysis_button = ttk.Button(frame1, command=lambda: launch_test(validate_action,
-                                                                        checkCompleteness_action,
-                                                                        checkFixity_action,
-                                                                        checkOrphanness_action),
-                                                                        text="4. Launch the test")
-launch_analysis_button.grid(row=9, column=0)
+    def save_report(self, report, IPs):
+        location = filedialog.asksaveasfilename(initialdir=path.curdir,
+                                                initialfile=f'Report_{strftime("%Y-%m-%dT%H:%M:%S%z", report.date)}.csv',
+                                                title = "Select the report location.",
+                                                filetypes = [("csv files", "*.csv")])
+        with open(location, 'w', newline='') as csvreport:
+            csvwriter = csv.writer(csvreport, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            report.actions.insert(0, 'Package')
+            csvwriter.writerow(report.actions)
+            for IP in IPs.list_of_ips:
+                row = [IP[0]]
+                if 'Validation' in report.actions:
+                    row.append(report.validation_report["/".join(IP)])
+                if 'Completeness check' in report.actions:
+                    row.append(report.completeness_report["/".join(IP)])
+                if 'Completeness and fixity check' in report.actions:
+                    row.append(report.completenessAndFixity_report["/".join(IP)])
+                if 'Orphanness check' in report.actions:
+                    row.append(report.orphanness_report["/".join(IP)])
+                csvwriter.writerow(row)
+
+    def report_callback_exception(self, exc, val, tb):
+
+        """Manages exceptions and returns them to the user in an error box"""
+        showerror("Error", message=str(val))
 
 # Additional code to correct blurry edges on Windows.
 # try:
@@ -207,4 +215,6 @@ launch_analysis_button.grid(row=9, column=0)
 
 #     windll.shcore.SetProcessDpiAwareness(1)
 # finally:
-root.mainloop()
+if __name__ == '__main__':
+    root = App()
+    root.mainloop()
